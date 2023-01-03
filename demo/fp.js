872b977710e6d0523d0046377691e3bf9b3f5359
LYC <liyichuan@baijia.com>
2023-01-03 11:20:53 +0800

    feat: add fp demo

---
+++ demo/fp.js
@@ -0,0 +1,46 @@
+const map = fn => array => array.map(fn);
+const prop = key => object => object[key];
+const reduce = (fn, initial) => array => array.reduce(fn, initial);
+const add = (x, y) => x + y;
+const sum = reduce(add, 0);
+const filter = fn => array => array.filter(fn);
+
+const average = items => (
+    item.length === 0
+    ? 0
+    : sum(items) / items.length
+);
+
+const flow = (...fns) => x => (
+    fns.reduce((result, fn) => fn(result), x)
+);
+
+const calcAvgCost = (items, filterFn = () => true) => (
+    flow(
+        filter(filterFn),
+        map(prop('price')),
+        average
+    )(items)
+);
+
+// 使用：
+const items = [
+    { name: 'Motherbord', manufacturer: 'A', price: 65 },
+    { name: 'CPU', manufacturer: 'A', price: 240 },
+    { name: 'DRAM', manufacturer: 'A', price: 100 },
+    { name: 'CPU', manufacturer: 'B', price: 150 },
+]
+
+const avgCost = calcAvgCost(items);
+
+const avgCostCPU = calcAvgCost(items, item => (
+    item.name === 'CPU'
+))
+
+const avgCostB = calcAvgCost(items, item => {
+    item.manufacturer === 'B'
+})
+
+const avgCostCPUFromA = calcAvgCost(items, item => (
+    item.name === 'CPU' && item.manufacturer === 'A'
+))

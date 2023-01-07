243686841fde45b034d799cda0819a7f846e66e0
LYC <liyichuan@baijia.com>
2023-01-07 13:31:07 +0800

    feat: add pm2

--- packages/pm2/app.js
+++ packages/pm2/app.js
@@ -16,6 +16,8 @@ app.use(ctx => {
         } catch(e) {
             // 因为是异步读取的，所以这里捕获不到readCallback函数中跑出的异常;
             // 但是可以捕获到上面👆try里抛出的异常
+            // Node.js里约定，同步代码才能捕获异常，异步代码不能直接使用 try/catch 
+            // (与你采用的异步流程控制方式有关，如果使用Promise，就使用Promise的异常处理方法)。
             console.log(`programe go catch: ${e}`)
         } finally {
             console.log('离开 try/catch');
